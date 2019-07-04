e618453cbbb38717b0bb72994882c47b0094b928
simon <simon_rekin@o2.pl>
2019-07-05 00:35:21 +0200

    added PlacesView view and templates refactoring

--- live_demo/urls.py
+++ live_demo/urls.py
@@ -1,9 +1,10 @@
 from django.urls import re_path
 
-from .views import AddPlaceView, ChangePlaceView
+from .views import AddPlaceView, ChangePlaceView, PlacesView
 
 urlpatterns = [
     re_path("^$", AddPlaceView.as_view(), name="add"),
     re_path("^places/(?P<pk>[0-9]+)/$", ChangePlaceView.as_view(), name="change"),
+    re_path("^index/$", PlacesView.as_view(), name="index"),
 
 ]
--- live_demo/views.py
+++ live_demo/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import CreateView, UpdateView
+from django.views.generic import CreateView, UpdateView, ListView
 
 from .models import Place
 
@@ -15,3 +15,7 @@ class ChangePlaceView(UpdateView):
     template_name = "live_demo/place_form.html"
     success_url = "/index/"
     fields = "__all__"
+
+class PlacesView(ListView):
+    model = Place
+    template_name = "live_demo/index.html"
