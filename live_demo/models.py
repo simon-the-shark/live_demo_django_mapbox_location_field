9eb4f7ceeed217a793ca31df4b345433950dea5f
simon <simon_rekin@o2.pl>
2019-11-03 19:54:49 +0100

    updated django-mapbox-location-field

--- live_demo/models.py
+++ live_demo/models.py
@@ -4,6 +4,6 @@ from mapbox_location_field.models import LocationField, AddressAutoHiddenField
 
 class Place(models.Model):
     location = LocationField(
-        map_attrs={"style": "mapbox://styles/mightysharky/cjwgnjzr004bu1dnpw8kzxa72"})
+        map_attrs={"style": "mapbox://styles/mightysharky/cjwgnjzr004bu1dnpw8kzxa72", 'center': [0, 100]})
     created_at = models.DateTimeField(auto_now_add=True)
     address = AddressAutoHiddenField()
