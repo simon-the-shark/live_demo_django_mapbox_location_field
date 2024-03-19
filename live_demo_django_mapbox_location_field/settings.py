5b6d5c61e3b36e0c540280ee0b038fa094b0bb48
Szymon Kowaliński <szymon@kowalinski.dev>
2024-03-19 01:35:15 +0100

    fixed BASE_DIR

--- live_demo_django_mapbox_location_field/settings.py
+++ live_demo_django_mapbox_location_field/settings.py
@@ -12,11 +12,12 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import sys
+from pathlib import Path
 
 from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
